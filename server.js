e364626a5048f0b62acd847d41135e2bf4bef72e
berriosa13 <berriosa13@gmail.com>
2021-08-14 13:13:03 -0400

    edited icons paths, package.json, server file, and manifest

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
-require("dotenv").config();
+if(process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
 const PORT = process.env.PORT || 5000;
 
 const cookieParser = require("cookie-parser");
